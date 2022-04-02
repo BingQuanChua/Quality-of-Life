72b2e50f921a5119b241204b8bfe4cb939b37d30
Liew Kuan Yung <liewkuanyung@gmail.com>
2022-04-02 20:54:32 +0800

    initialize react

---
+++ react-app/src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import DemoChart from './components/DemoChart';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <DemoChart/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-app/src/components/DemoChart.js
@@ -0,0 +1,35 @@
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
+
+const data = [
+  { name: "A", uv: 400, pv: 2400, amt: 2400 },
+  { name: "B", uv: 300, pv: 2400, amt: 2400 },
+  { name: "C", uv: 350, pv: 2400, amt: 2400 },
+  { name: "D", uv: 200, pv: 2400, amt: 2400 },
+  { name: "E", uv: 100, pv: 2400, amt: 2400 },
+];
+
+const renderLineChart = () => {
+  return (
+    <LineChart
+      width={400}
+      height={250}
+      data={data}
+      margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+    >
+      <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+    </LineChart>
+  );
+};
+
+export default renderLineChart;
