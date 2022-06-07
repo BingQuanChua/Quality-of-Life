2bfeec38b4029bc993acae4e2fd6f2c59d1cb094
BingQuan <bingquan6102@gmail.com>
2022-06-07 23:52:19 +0800

    feat: Minor polishing

--- react-app/src/components/QualityOfLife/PieRechartsLegend.js
+++ react-app/src/components/QualityOfLife/PieRechartsLegend.js
@@ -11,12 +11,12 @@ const color_gender = ['#2196f3', '#e91e63']
 export default function PieRechartsLegend() {
   return (
     <Stack spacing={0.5}>
-      <Item sx={{backgroundColor: color_age[0], pl:2}}>18-24_years_old</Item>
-      <Item sx={{backgroundColor: color_age[1], pl:2}}>25-34_years_old</Item>
-      <Item sx={{backgroundColor: color_age[2], pl:2}}>35-44_years_old</Item>
-      <Item sx={{backgroundColor: color_age[3], pl:2}}>45-54_years_old</Item>
-      <Item sx={{backgroundColor: color_age[4], pl:2}}>55-64_years_old</Item>
-      <Item sx={{backgroundColor: color_age[5], pl:2}}>65-74_years_old</Item>
+      <Item sx={{backgroundColor: color_age[0], pl:2}}>18-24 years old</Item>
+      <Item sx={{backgroundColor: color_age[1], pl:2}}>25-34 years old</Item>
+      <Item sx={{backgroundColor: color_age[2], pl:2}}>35-44 years old</Item>
+      <Item sx={{backgroundColor: color_age[3], pl:2}}>45-54 years old</Item>
+      <Item sx={{backgroundColor: color_age[4], pl:2}}>55-64 years old</Item>
+      <Item sx={{backgroundColor: color_age[5], pl:2}}>65-74 years old</Item>
       <Item sx={{backgroundColor: color_age[6], pl:2}}>75+</Item>
       <Item sx={{backgroundColor: color_gender[0], pl:2}}>Male</Item>
       <Item sx={{backgroundColor: color_gender[1], pl:2}}>Female</Item>
